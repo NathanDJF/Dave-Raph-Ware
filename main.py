550373a93ca898e6dfb11191be9fa61bea3a75c1
NathanDJF <nathandjfcodes@gmail.com>
2024-07-30 20:31:40 +0700

    third commit (main loop, main_menu, icon)

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-# test
+import pygame
+import sys
+import os
 
-# bro is github even responding
+pygame.init()
+pygame.mixer.init()
+
+# images
+main_menu_img = pygame.image.load(os.path.join('Assets/main menu.png'))
+icon_img = pygame.image.load(os.path.join('Assets/icon.png'))
+
+# vayriables
+on_main_menu = True
+
+# screenie
+screen = pygame.display.set_mode((1200, 600))
+pygame.display.set_caption('Dave Raph Ware')
+pygame.display.set_icon(icon_img)
+
+# main loop
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+    if on_main_menu:
+        screen.blit(main_menu_img, (0, 0))
+    if pygame.mouse.get_pressed()[0] == 1:
+        screen.fill((255, 255, 255))
+        on_main_menu = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
